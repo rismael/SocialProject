8808131d166393ba7d8849a2cbbb204d07f2b9e2
Ismael Rodriguez <ir3987@tamu.edu>
2020-11-06 23:42:15 -0600

    Incorporated Twit with config file for tokens/keys

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('Does this work? Yes!')
+console.log('Does this work? Yes! Running...')
 var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+
+
+
+console.log('Stopping...');
