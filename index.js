c84b03dd598afc926e6e1c8ee09ba46f6b150fa5
Ismael Rodriguez <ir3987@tamu.edu>
2020-11-10 00:43:41 -0600

    Added the ability to get tweet text. Fixed a bug where tweets that were retweeted had truncated text. Tweet text is sent to a file called output.txt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log('Does this work? Yes! Running...')
 var Twit = require('twit');
+const fs = require('fs');
 
 var config = require('./config');
 var T = new Twit(config);
@@ -15,8 +16,23 @@ function gotData(err, data, response)
     //console.log(data);
     for(var i = 0; i < data.length; i++)
     {
-        console.log(data[i].full_text);
-        //console.log('\n')
+        var tweet;
+        if(data[i].retweeted_status)
+        {
+            tweet = data[i].retweeted_status.full_text;
+        }
+        else
+        {
+            tweet = data[i].full_text;
+        }
+
+        console.log("- " + tweet);
+        //fs.appendFileSync('output.txt', 'Untouched- ' + data[i].full_text + '\n', handleMe);
+        fs.appendFileSync('output.txt', tweet + '\n', handleMe);
+        function handleMe(err)
+        {
+            if(err) console.log('err');
+        }
     }
 }
 
