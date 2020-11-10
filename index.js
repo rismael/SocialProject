e9e46ae81ea94d050e10d1d03d412b9fd17b23eb
Ismael Rodriguez <ir3987@tamu.edu>
2020-11-09 19:12:30 -0600

    Added code to handle the API. Allowing to read the home timeline. The text of the tweets are then outputted to the terminal.

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
+var params = { count: 3,
+                "tweet_mode" : "extended"
+            };
+
+T.get('statuses/home_timeline', params, gotData);
+
+function gotData(err, data, response)
+{
+    //console.log(data);
+    for(var i = 0; i < data.length; i++)
+    {
+        console.log(data[i].full_text);
+        //console.log('\n')
+    }
+}
 
 
 
