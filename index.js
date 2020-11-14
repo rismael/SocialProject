eb9836f8a10a1143e5b2aac36997f3e62e002085
Ismael Rodriguez <ir3987@tamu.edu>
2020-11-13 23:18:15 -0600

    Added comments to index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-console.log('Does this work? Yes! Running...')
+console.log('Running...')
+//This code uses 'twit' as the API Library simplifying the use of Twitter's API
 var Twit = require('twit');
+//fs is used for writing to files
 const fs = require('fs');
-
+//config file is used to access Tokens/Keys
 var config = require('./config');
 var T = new Twit(config);
-
+//parameters for the query
 var params = { count: 3,
                 "tweet_mode" : "extended"
             };
-
+//We want to get User's timeline. Response is sent to the function gotData
 T.get('statuses/home_timeline', params, gotData);
 
+//gotData processes the response and gets/displays the text of the tweets
 function gotData(err, data, response)
 {
     //console.log(data);
     for(var i = 0; i < data.length; i++)
     {
         var tweet;
+        //tweets that are Retweets need extra handling
         if(data[i].retweeted_status)
         {
             tweet = data[i].retweeted_status.full_text;
@@ -35,7 +39,3 @@ function gotData(err, data, response)
         }
     }
 }
-
-
-
-console.log('Stopping...');
