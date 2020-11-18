8c450a3bde1de56d673eb017d37ddf1cd262ded2
Ismael Rodriguez <ir3987@tamu.edu>
2020-11-17 23:11:03 -0600

    Added color styling and moved FB warning to the center. Added FB warning in index.pug.

--- nodejs-https-server/routes/index.js
+++ nodejs-https-server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HELLO' });
+  res.render('index', { title: 'Text and Photo Matcher' });
 });
 
 module.exports = router;
