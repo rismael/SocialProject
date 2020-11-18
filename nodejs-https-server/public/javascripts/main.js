708aad95716065cdd9ed346e1765e42dca7513e6
Ismael Rodriguez <ir3987@tamu.edu>
2020-11-17 23:54:57 -0600

    Main.js, created function to create the instagram auth link

---
+++ nodejs-https-server/public/javascripts/main.js
@@ -0,0 +1,12 @@
+var config = require('../../keys/insta_config');
+const querystring = require('querystring');
+var api_url = 'https://api.instagram.com/'
+
+
+function makeAuthLink(){
+    var link = api_url + 'oauth/authorize?' + querystring.stringify({client_id : config.app_id, redirect_uri : config.redirect_uri, scope : 'user_profile,user_media', response_type: 'code'});
+    link = querystring.unescape(link);
+    console.log(link);
+}
+
+makeAuthLink();
