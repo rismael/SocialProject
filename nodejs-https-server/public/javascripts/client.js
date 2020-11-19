6f95dbf537d77a9a9d945de4394e9b8740f99216
Ismael Rodriguez <ir3987@tamu.edu>
2020-11-18 23:05:01 -0600

    Created prototype to send requests to server. That way in the future the server can process the request and send back the appropriate data.

--- nodejs-https-server/public/javascripts/client.js
+++ nodejs-https-server/public/javascripts/client.js
@@ -1,5 +1,29 @@
 var xhttp = new XMLHttpRequest();
 console.log('AJAX is running');
-if(!xhttp) {
-    console.log('Failed creating XMLHttp instance.');
+
+
+function make_request(){
+    if(!xhttp) {
+        console.log('Failed creating XMLHttp instance.');
+        return false;
+    }
+    xhttp.onreadystatechange = update_ig_auth_link;
+    xhttp.open('GET', 'users');
+    xhttp.send();
+}
+
+function update_ig_auth_link() {
+    try{
+        if(xhttp.readyState == XMLHttpRequest.DONE){
+            if(xhttp.status == 200){
+                alert('Time to modify href >:)');
+            }
+            else{
+                console.log('Problem with the Request');
+            }
+        }
+    }
+    catch(e){
+        console.log('Caught exception: ' + e.description);
+    }
 }
